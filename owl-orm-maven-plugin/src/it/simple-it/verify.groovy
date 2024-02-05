5558e5c03074635f3d7177c7a79dc909bffa72df
ben.gould <ben.gould@inovexcorp.com>
2024-02-05 15:19:55 -0500

    further design of abstracting generation out of the maven plugin directly (for testability and extensibility)

--- owl-orm-maven-plugin/src/it/simple-it/verify.groovy
+++ owl-orm-maven-plugin/src/it/simple-it/verify.groovy
@@ -1,3 +1,10 @@
-File touchFile = new File( basedir, "target/touch.txt" );
+/**
+ * Groovy test for maven generation plugin execution.
+ */
 
-assert touchFile.isFile()
+File bieronto = new File(basedir, "target/generated-sources/com/realmone/bieronto")
+assert bieronto.isDirectory()
+assert bieronto.listFiles().length == 19
+File foaf = new File(basedir, "target/generated-sources/com/realmone/foaf")
+assert foaf.isDirectory()
+assert foaf.listFiles().length == 15
