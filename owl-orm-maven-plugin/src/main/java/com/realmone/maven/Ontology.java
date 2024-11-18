c732f743025db0a7c5b756000bcd8ce237dae623
ben.gould <ben.gould@realmone.com>
2024-11-18 12:23:16 -0500

    removed unused

--- owl-orm-maven-plugin/src/main/java/com/realmone/maven/Ontology.java
+++ owl-orm-maven-plugin/src/main/java/com/realmone/maven/Ontology.java
@@ -17,29 +17,8 @@
  */
 package com.realmone.maven;
 
-import com.realmone.owl.orm.OrmException;
-import com.realmone.owl.orm.generate.OntologyMeta;
-import com.sun.codemodel.JDefinedClass;
 import lombok.Getter;
-import lombok.NonNull;
-import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.FileSystemManager;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.model.ModelFactory;
-import org.eclipse.rdf4j.model.Resource;
-import org.eclipse.rdf4j.model.Value;
-import org.eclipse.rdf4j.model.impl.DynamicModelFactory;
-import org.eclipse.rdf4j.model.vocabulary.OWL;
-import org.eclipse.rdf4j.model.vocabulary.RDF;
-import org.eclipse.rdf4j.model.vocabulary.RDFS;
-import org.eclipse.rdf4j.rio.RDFFormat;
-import org.eclipse.rdf4j.rio.Rio;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
-import java.util.stream.Collectors;
 
 @Getter
 public class Ontology {
