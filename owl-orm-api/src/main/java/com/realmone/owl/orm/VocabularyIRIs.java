0da2a56eaae0655abe2ca9b2afe9db444774cd7d
ben.gould <ben.gould@realmone.com>
2025-04-07 18:15:40 -0400

    use lombok UtilityClass annotation vs private constructor

--- owl-orm-api/src/main/java/com/realmone/owl/orm/VocabularyIRIs.java
+++ owl-orm-api/src/main/java/com/realmone/owl/orm/VocabularyIRIs.java
@@ -1,5 +1,6 @@
 package com.realmone.owl.orm;
 
+import lombok.experimental.UtilityClass;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.base.InternedIRI;
 
@@ -7,9 +8,8 @@ import org.eclipse.rdf4j.model.base.InternedIRI;
  * A utility class to create IRIs for use in generated interfaces so clients can do things like `Beer.TYPE` to get the
  * IRI of the Beer class or `Beer.ALCOHOLBYVOLUME` to get the IRI of the alcoholByVolume property.
  */
+@UtilityClass
 public class VocabularyIRIs {
-    private VocabularyIRIs() {
-    }
 
     public static IRI createIRI(String namespace, String localName) {
         return new InternedIRI(namespace, localName);
